215ddaf2c251bd7f5682e49e17eabb7fe9379f55
KLoveEvangelista <kryztleloveevangelista@gmail.com>
2019-07-09 16:24:22 +0800

    initial

---
+++ Book System/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -0,0 +1,27 @@
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-fetch-data',
+  templateUrl: './fetch-data.component.html'
+})
+export class FetchDataComponent {
+  public forecasts: WeatherForecast[];
+
+  nums: number[] = [1,2,3,4,5];
+  name: string;
+
+  constructor(http: HttpClient) {
+    http.get<WeatherForecast[]>('api/SampleData/WeatherForecasts').subscribe(result => {
+      this.forecasts = result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  dateFormatted: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+  
+}
---
+++ Book System/Controllers/MySampleController.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel.DataAnnotations;    
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Book_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MySampleController : Controller
+    {
+
+        // api/MySample/MyName (case insensitive)
+        [HttpGet("[action]")]
+        public ActionResult<MyResponse> MyName()
+        {
+            return Ok(new MyResponse { IsSuccess = true, Message = "Hi! I'm John Rey" });
+        }
+
+        // api/MySample/YourName/{name}
+        [HttpGet("[action]/{name}")]
+        public ActionResult<MyResponse> YourName(string name)
+        {
+            // string interpolation the same as "Hi " + name
+            return Ok(new MyResponse { IsSuccess = true, Message = $"Hi {name}" });
+        }
+
+        // api/MySample/LegalAge
+        [HttpPost("[action]")]
+        public ActionResult<MyResponse> LegalAge([FromBody] Person person)
+        {
+            MyResponse response;
+            if (!ModelState.IsValid)
+            {
+                response = new MyResponse { IsSuccess = false, Message = "Error" };
+                return BadRequest(response);
+            }
+
+            if (person.Age < 18)
+            {
+                response = new MyResponse { IsSuccess = false, Message = "Not legal age" };
+                return BadRequest(response);
+
+            }
+            else if (!person.Name.ToLower().Contains("x"))
+            {
+                response = new MyResponse { IsSuccess = false, Message = "x is not found in the name string" };
+                return NotFound(response);
+            }
+
+            response = new MyResponse { IsSuccess = true, Message = "Success" };
+            return Ok(response);
+        }
+
+        // api/MySample/GetBooks
+        //[HttpGet("[action]")]
+        //public ActionResult<IEnumerable<Book>> GetBooks()
+        //{
+        //    return _context.Books.ToList();
+        //}
+
+        public class Person
+        {
+            // more on data annotations later which can be displayed error message in ModelState.IsValid
+            [Required]
+            [RegularExpression("^[a-zA-Z0-9_]*$", ErrorMessage = "No special characters allowed")]
+            public string Name { get; set; }
+
+            [Required]
+            [Range(18, 90, ErrorMessage = "Age should be between 18 to 90!")]
+            public int Age { get; set; }
+        }
+
+        public class MyResponse
+        {
+            public bool IsSuccess { get; set; }
+            public string Message { get; set; }
+        }
+    }
+}
